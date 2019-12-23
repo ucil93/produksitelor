654fb46b968f380b22f687c0551e9fa0f8ef48a3
miftahol <lmiftahol@gmail.com>
2019-12-23 21:36:38 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ $config['modules_locations'] = array(
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '/produksite';
+$config['base_url'] = '/produksitelor';
 
 /*
 |--------------------------------------------------------------------------
